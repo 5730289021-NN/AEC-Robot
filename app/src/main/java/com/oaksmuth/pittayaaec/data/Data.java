725074a5922f99be4f3e1804324b825942e9d931
Oak <norawitn@hotmail.com>
2016-03-06 18:34:56 +0700

    Changed to SugarORM db.

---
+++ app/src/main/java/com/oaksmuth/pittayaaec/data/Data.java
@@ -0,0 +1,31 @@
+package com.oaksmuth.pittayaaec.data;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by Oak on 6/3/2559.
+ */
+public class Data extends SugarRecord {
+    //ID	Topic	Subtopic	Number	Question	Answer	English 1	Thai 1	English 2	Thai 2	English 3	Thai 3	English 4	Thai 4	English 5	Thai 5
+    private long ID;
+
+    public Long getId() {
+        return ID;
+    }
+
+    String Topic;
+    String SubTopic;
+    int Number;
+    String Question;
+    String Answer;
+    String English1;
+    String Thai1;
+    String English2;
+    String Thai2;
+    String English3;
+    String Thai3;
+    String English4;
+    String Thai4;
+    String English5;
+    String Thai5;
+}
