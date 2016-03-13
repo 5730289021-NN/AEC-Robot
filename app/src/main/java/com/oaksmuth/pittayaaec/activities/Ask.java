bb196594d1312686859831b14055dd9c671f553e
noraw <norawitn@hotmail.com>
2016-03-13 22:44:48 +0700

    Ask function is finished

--- app/src/main/java/com/oaksmuth/pittayaaec/activities/Ask.java
+++ app/src/main/java/com/oaksmuth/pittayaaec/activities/Ask.java
@@ -8,16 +8,19 @@ import android.graphics.AvoidXfermode;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.oaksmuth.pittayaaec.R;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -31,15 +34,19 @@ public class Ask extends AppCompatActivity{
     private String said;
     private String fromDB;
     private Context context;
-    private Context resultContext;
     private Cursor cursor;
+    private TextToSpeech tts;
+    private TextView speedTextView;
+    private TextView pitchTextView;
+    private DecimalFormat df;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = this;
+        setContentView(R.layout.activity_ask);
         linearLayout = (LinearLayout) findViewById(R.id.ask_textLayout);
         scrollView = (ScrollView) findViewById(R.id.ask_scrollView);
-        setContentView(R.layout.activity_ask);
         Button askButton = (Button) findViewById(R.id.ask_button);
         askButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,22 +66,57 @@ public class Ask extends AppCompatActivity{
                 }
             }
         });
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                tts.speak("Hello, ask me whatever you want to know",TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+        speedTextView = (TextView) findViewById(R.id.ask_speedtextView);
+        pitchTextView = (TextView) findViewById(R.id.ask_pitchtextView);
+        df = new DecimalFormat("0.00");
+        SeekBar speedSeekBar = (SeekBar) findViewById(R.id.ask_speedSeekbar);
+        speedSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float speedValue = Float.parseFloat(df.format((float) (Math.pow(2, (double) progress / 50) / 2)));
+                speedTextView.setText("Speed\t\t" + String.valueOf(speedValue) + "\t");
+                tts.setSpeechRate(speedValue);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        SeekBar pitchSeekBar = (SeekBar) findViewById(R.id.ask_pitchSeekbar);
+        pitchSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float pitchValue = Float.parseFloat(df.format((float) (Math.pow(2, (double) progress/50)/2)));
+                pitchTextView.setText("Pitch\t\t" + String.valueOf(pitchValue) + "\t");
+                tts.setPitch(pitchValue);
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        resultContext = this;
         switch (requestCode) {
             case SPEECH_RECOGNITION_CODE: {
                 if (resultCode == RESULT_OK && null != data) {
                     final ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    Toast.makeText(this, said, Toast.LENGTH_LONG).show();
                     said = result.get(0);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            TextView textView = new TextView(resultContext);
-                            textView.setText(said);
+                            TextView textView = new TextView(context);
+                            textView.setText(simplifyText(said)[0] + "\n");
                             textView.setTextColor(Color.BLACK);
                             linearLayout.addView(textView);
                             scrollView.fullScroll(View.FOCUS_DOWN);
@@ -89,11 +131,12 @@ public class Ask extends AppCompatActivity{
                         cursor.moveToFirst();
                         fromDB = cursor.getString(0);
                     }
+                    tts.speak(fromDB,TextToSpeech.QUEUE_FLUSH,null);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             TextView textView = new TextView(context);
-                            textView.setText(fromDB);
+                            textView.setText(fromDB + "\n");
                             linearLayout.addView(textView);
                             scrollView.fullScroll(View.FOCUS_DOWN);
                         }
@@ -108,9 +151,11 @@ public class Ask extends AppCompatActivity{
         text = text.trim();
         if(text.endsWith("?"))
         {
-            text.subSequence(0,text.length() - 1);
+            text = text.substring(0,text.length() - 1);
         }
-        text = text + " ?";
+        String cap = String.valueOf(text.charAt(0)).toUpperCase();
+        text = text.substring(1,text.length());
+        text = cap + text + " ?";
         ans[0] = text;
         return ans;
     }
