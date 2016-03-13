718cc4bab36851ebe26146eee4d00423927104b2
Oak <norawitn@hotmail.com>
2016-03-13 16:28:50 +0700

    Add ask module

---
+++ app/src/main/java/com/oaksmuth/pittayaaec/activities/Ask.java
@@ -0,0 +1,15 @@
+package com.oaksmuth.pittayaaec.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Oak on 13/3/2559.
+ */
+public class Ask extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
